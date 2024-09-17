a28cd96bcceb99f3cd3e1e5f90989f3375b73ec4
ogu1101 <sogura1101@gmail.com>
2024-09-17 23:02:05 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "ecr_repository_url" {
-  value       = aws_ecr_repository.repository.repository_url
-  description = "ECR Repository URL"
-}
-
 output "aws_account_id" {
   value       = data.aws_caller_identity.current.account_id
   description = "AWS Account ID"
